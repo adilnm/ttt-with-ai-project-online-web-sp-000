38f31f3309462bf1215d399b82fc5b53d6dbf2ff
ADIL NAIMI <adilnm23@hotmail.com>
2019-04-09 02:35:04 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,110 +1,119 @@
-class Game
-  WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-  attr_accessor :board, :player_1, :player_2
-  attr_reader:player_1
-  
-  def initialize(player_1 = Players::Human.new("X"),player_2 = Players::Human.new("O"),board=Board.new)
-    @player_1 = player_1
-    @player_2=player_2
-    @board=board
+WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
   end
-  
-  def current_player
-    self.board.turn_count.even? ? @player_1 : @player_2
-  end
-  
-  def won?
-    win=false
-    WIN_COMBINATIONS.each do |win_comb|
-      if(@board.cells[win_comb[0]]=="X" && @board.cells[win_comb[1]]=="X" && @board.cells[win_comb[2]]=="X")
-        win=win_comb
-      elsif(@board.cells[win_comb[0]]=="O" && @board.cells[win_comb[1]]=="O" && @board.cells[win_comb[2]]=="O")
-        win=win_comb
-     end
-    end
-    win
-  end
-  
-  def draw?
-<<<<<<< HEAD
-    !won? && board.full?
-  end
-  
-  def over?
-    won? || draw? ? true : false
-=======
-    !won?
-  end
-  
-  def over?
-    if(@board.full?)
-      true
-    else 
-      false
-    end
->>>>>>> 59e2b61a9214d3ad623e6be6560b56d0c1703438
-  end
-  
-  def winner
-    if won?
-      return @board.cells[won?[0]]
-    end
-  end
-  
-  def turn
-    player=current_player
-<<<<<<< HEAD
-    puts"Player #{player.token}, please entre a number between 1 and 9"
-=======
-    puts"#{player}, please entre a number between 1 and 9"
->>>>>>> 59e2b61a9214d3ad623e6be6560b56d0c1703438
-    input=player.move(board)
-    if(!board.valid_move?(input))
-      puts "That input is invalid"
-      turn
+end
+
+def turn_count(board)
+  count=0
+  board.each do |space|
+    if(space=="X"||space=="O")
+      count+=1
+end
+end
+return count
+
+end
+
+def current_player(board)
+  number=turn_count(board)
+
+    if(number%2==0)
+      return "X"
     else
-      board.update(input,player)
-       board.display
+      return "O"
+    
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each { |win_comb|
+    if(board[win_comb[0]]=="X" && board[win_comb[1]]=="X" && board[win_comb[2]]=="X")
+      return win_comb
+      elsif(board[win_comb[0]]=="O" && board[win_comb[1]]=="O" && board[win_comb[2]]=="O")
+      return win_comb
     end
+  } 
+      return false
+  
+end
+
+def full?(board)
+  board.all? {|position|
+  position=="X" || position=="O"
+  }
+end
+
+def draw?(board)
+  if(full?(board)&&!won?(board))
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if(won?(board)||draw?(board)||full?(board))
+    return true
+  else
+    return false
   end
   
-  def play
-<<<<<<< HEAD
-     until over?
-     self.turn
-     end
-     
-     if won?
-       puts"Congratulations #{winner}!"
-     end
-     
-     if draw?
-       puts"Cat's Game!"
-     end
-     
-  end
-    
 end
-=======
-    until over?
-      turn
-    end
 
-    if won?
-      puts "\n"
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "\n"
-      puts "Cat's Game!"
-    end
-    board.display
+def winner(board)
+  if(won?(board)==false)
+    return nil
+  elsif(board[won?(board)[0]]=="X")
+    return "X"
+    else(board[won?(board)[0]]=="O")
+    return "O"
+ 
   end
-    
 end
 
 
 
 
+def play(board)
+  until over?(board) == true
+    turn(board)
+  end
 
-#learn spec/04_game_spec.rb
->>>>>>> 59e2b61a9214d3ad623e6be6560b56d0c1703438
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
